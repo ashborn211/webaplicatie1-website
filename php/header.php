e065cf8e105e096f97b6f6fec785b590db2b7004
velican <120579492+ashborn211@users.noreply.github.com>
2023-04-17 13:58:57 +0200

    just made an delete and create for admin

---
+++ php/header.php
@@ -0,0 +1,11 @@
+<header>
+<nav>
+    <ul>
+        <li><a href="index.php">home</a></li> 
+        <li><a href="bestellen.php">bestellen</a></li>    
+        <li><a href="menu.php">menu</a></li>
+        <li><a href="login.php">inloggen</a></li>
+        <li><a href="contact.php">contact us</a></li>
+    </ul>
+    </nav>
+</header>
